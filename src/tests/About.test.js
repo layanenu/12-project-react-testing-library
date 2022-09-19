e2847bd6f786d1cb1d6e54667d2803ade67fdc8f
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-09-19 15:50:13 -0300

    Requisito 2 OK

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,30 @@
-// import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
 
-test('', () => {});
+test('Teste se a página contém as informações sobre a Pokédex', () => {
+  renderWithRouter(<About />);
+
+  const aboutTittle = screen.getByRole(
+    'heading',
+    { name: /About Pokédex/i },
+    { level: 2 },
+  );
+
+  const imgAboutPokedex = screen.getByRole('img', { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
+
+  const firstParagraph = screen
+    .getByText(
+      /this application simulates a pokédex, a digital encyclopedia containing all/i,
+    );
+  const secondParagraph = screen
+    .getByText(/one can filter pokémons by type, and see more details for each/i);
+
+  expect(aboutTittle).toBeInTheDocument();
+  expect(firstParagraph).toBeInTheDocument();
+  expect(secondParagraph).toBeInTheDocument();
+  expect(imgAboutPokedex).toBeInTheDocument();
+  expect(imgAboutPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
