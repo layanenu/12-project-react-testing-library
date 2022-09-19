5ac6f965974d78add567430935be7fe19bb48785
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-09-19 16:38:21 -0300

    Requisito 3 OK

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const paragraphPageFavorites = screen.getByText(/no favorite pokemon found/i);
+  expect(paragraphPageFavorites).toBeInTheDocument();
+});
