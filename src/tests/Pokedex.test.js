60a3b07c84ffdfe559d445703c0c9d3c5f4b142b
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-09-19 18:46:26 -0300

    Requisito 5 OK

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Verifica se a Pokédex tem os botões de filtro', () => {
+  renderWithRouter(<App />);
+  const pokemonsTypes = [
+    'Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+  const getPokemonForButtonType = screen.getAllByTestId('pokemon-type-button');
+  getPokemonForButtonType.forEach((event, index) => {
+    expect(event).toHaveTextContent(pokemonsTypes[index]);
+  });
+});
+test('Verifica se a Pokédex contém um botão para resetar o filtro', () => {
+  renderWithRouter(<App />);
+  const buttonAll = screen.getByRole('button', { name: /all/i });
+  expect(buttonAll).toBeInTheDocument();
+  userEvent.click(buttonAll);
+});
